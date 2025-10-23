3224ad16233b965afd3515fba4f5cc80f3b0afa2
Thomas Pal <thomas.pal@tuhh.de>
2025-10-23 20:41:19 +0200

    Atrion: Remove separate kotlin frontend implementation

--- frontend/composeApp/src/commonMain/kotlin/dev/tjpal/Platform.kt
+++
@@ -1,7 +0,0 @@
-package dev.tjpal
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- frontend/composeApp/src/jsMain/kotlin/dev/tjpal/Platform.js.kt
+++
@@ -1,7 +0,0 @@
-package dev.tjpal
-
-class JsPlatform: Platform {
-    override val name: String = "Web with Kotlin/JS"
-}
-
-actual fun getPlatform(): Platform = JsPlatform()
