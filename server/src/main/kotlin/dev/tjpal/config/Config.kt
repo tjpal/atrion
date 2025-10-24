6eef8b804a7862bcc62b2a5987676953ac563c09
tjpal <50217098+tjpal@users.noreply.github.com>
2025-10-24 19:32:47 +0200

    Add initial config file

---
+++ server/src/main/kotlin/dev/tjpal/config/Config.kt
@@ -0,0 +1,10 @@
+package dev.tjpal.config
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Config(
+    val httpHost: String,
+    val httpPort: Int,
+    val udsPath: String
+)
---
+++ server/src/main/kotlin/dev/tjpal/config/ConfigLoader.kt
@@ -0,0 +1,30 @@
+package dev.tjpal.config
+
+import kotlinx.serialization.json.Json
+import java.io.File
+import javax.inject.Inject
+import javax.inject.Named
+
+class ConfigLoader @Inject constructor(
+    @param:Named("configPath") private val configPath: String,
+    private val json: Json
+) {
+    private val default = Config(
+        httpHost = "0.0.0.0",
+        httpPort = 8081,
+        udsPath = "/tmp/atrion.socket"
+    )
+
+    fun loadOrCreate(): Config {
+        val file = File(configPath)
+        return try {
+            val text = file.readText()
+            json.decodeFromString<Config>(text)
+        } catch (e: Exception) {
+            println("Error reading config: ${e.message}. Creating default file. Please review and restart.")
+            file.parentFile?.mkdirs()
+            file.writeText(json.encodeToString(default))
+            default
+        }
+    }
+}
