6a6c2a678419ad10c61ce875282799a5b533acbf
tjpal <50217098+tjpal@users.noreply.github.com>
2026-01-06 12:37:11 +0100

    Start Atrion editor by default in HD resolultion

--- composeApp/src/jvmMain/kotlin/dev/tjpal/main.kt
+++ composeApp/src/jvmMain/kotlin/dev/tjpal/main.kt
@@ -1,7 +1,9 @@
 package dev.tjpal
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import dev.tjpal.di.listModules
 import dev.tjpal.di.listProperties
 import org.koin.core.context.startKoin
@@ -15,6 +17,7 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "atrion",
+        state = rememberWindowState(width = 1920.dp, height = 1080.dp)
     ) {
         App()
     }
